9e28a156a3ab46b4a747f90401f36e9c2375b3ca
Julian-Webb <julian.webb00+github@gmail.com>
2024-03-12 10:54:32 +0100

    Added functions to create ROIs: generate_rois_from_size and save_rois

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
-import sys
 import os
+from generate_rois import generate_rois_from_size, save_rois
 
-sys.path.append(os.getcwd() + '/1_data_input')
-from input_tif import image_as_array
+# base_path = '/Users/julian/development/PycharmProjects/glioblastoma/'
+# calcium_img_path = 'data/01_raw/276_AZD3965_Mathieu.TIF'
+# test_img_path = 'data/01_raw/test_stack/TestStack.tif'
+# simple_test_stack = 'data/01_raw/simple_test_image/simple_test_stack.tif'
+#
+# image_array = image_as_array(base_path + test_img_path)
 
-# TODO optimize folder structure and imports
+test_stack_dir = '/Users/julian/development/PycharmProjects/glioblastoma/data/01_raw/high_res_test_stack/'
+image_path = os.path.join(test_stack_dir, 'high_res_test_stack.tif')
+
+# Step 1 & 2: Load the image and compute the regions of interest (ROIs)
+generated_rois = generate_rois_from_size(image_path, 512, 256)
+save_rois(generated_rois, test_stack_dir)
+
+# Step 3: Generate the signals based on the ROIs
 
 
-base_path = '/Users/julian/development/PycharmProjects/glioblastoma/'
-calcium_img_path = 'data/01_raw/276_AZD3965_Mathieu.TIF'
-test_img_path = 'data/01_raw/test_stack/TestStack.tif'
-simple_test_stack = 'data/01_raw/simple_test_image/simple_test_stack.tif'
+# Step 4: Estimate which signals are duplicates
 
-image_array = image_as_array(base_path + test_img_path)
+# Step 5: Filter out duplicate signals and save the result
 
